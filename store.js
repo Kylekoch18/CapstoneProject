2798d966936f8b8ed7c58f377595812520ccafad
Kyle Koch <kylekoch18@gmail.com>
2019-10-01 00:44:16 -0500

    add shopping cart baseline to project.

---
+++ store.js
@@ -0,0 +1,26 @@
+ //create a checkout button
+<a href="javascript:;" class="cart_checkout"></a>
+
+// button to empty the cart
+<a href="javascript:;" class="cart_empty"></a>
+
+//show the cart
+<div class="cart_items"></div>
+
+//cart total (ex. $23.11)
+<div class="cart_total"></div>
+
+//cart quantity (ex. 3)
+<div class="cart_quantity"></div>
+
+//tax cost (ex. $1.38)
+<div class="cart_tax"></div>
+
+//tax rate (ex. %0.6) 
+<div class="cart_taxRate"></div>
+
+//shipping (ex. $5.00) 
+<div class="cart_shipping"></div>
+
+//grand total, including tax and shipping (ex. $28.49) 
+<div class="cart_grandTotal"></div>
